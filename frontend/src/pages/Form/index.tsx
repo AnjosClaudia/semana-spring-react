c161284fa19d55470752ad30145198c7567b0aa1
Claudia dos Anjos <claudia.anjos@ifood.com.br>
2022-05-11 18:33:44 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+function Form(){
+
+    return (
+        <h1>Oi</h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,8 @@
+function Listing(){
+
+    return (
+        <h1>Oi</h1>
+    );
+}
+
+export default Listing;
