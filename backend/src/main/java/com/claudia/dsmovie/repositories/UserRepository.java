70d618a40f9f4db2fe715efb3baea3e0c6739572
Claudia dos Anjos <claudia.anjos@ifood.com.br>
2022-05-12 10:45:08 -0300

    Find Movies

---
+++ backend/src/main/java/com/claudia/dsmovie/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.claudia.dsmovie.repositories;
+
+import com.claudia.dsmovie.entities.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
